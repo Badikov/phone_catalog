97d23cc6166e3e3fb86e347c905c8baec24e41dd
Pavel Nemkin <byte.developer@gmail.com>
2012-01-19 18:28:33 +0600

    Type and types data

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120119120659) do
+ActiveRecord::Schema.define(:version => 20120119122011) do
 
   create_table "case_types", :force => true do |t|
+    t.string "name", :null => false
+  end
+
+  create_table "types", :force => true do |t|
     t.string "name", :null => false
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 # encoding: utf-8
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 # Vendors
 File.open('lib/phone_data/vendor_list.txt').each_line do |vendor_name|
   Vendor.create(name: vendor_name)
@@ -16,4 +8,7 @@ end
 #Case types
 CaseType.create([{name: 'классический'}, {name: 'раскладушка'}, {name: 'ротатор'},
                  {name: 'с откидной крышкой'}, {name: 'слайдер'}, {name: 'часы'}])
+
+# Types
+Type.create([{name: 'смартфон/коммуникатор'}, {name: 'телефон'}])
 
