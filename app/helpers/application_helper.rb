aaa846d362e78efa99a4207be217276307168ae5
kanfet <byte.developer@gmail.com>
2012-06-21 18:34:24 +0600

    delete opening slash from root path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
 
   def relative_url_root
-    "#{ActionController::Base.config.relative_url_root}/"
+    root = ActionController::Base.config.relative_url_root
+    root = root[1..root.length] if root
+    "#{root}/"
   end
 end
