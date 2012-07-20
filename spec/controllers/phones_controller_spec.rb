75f99c6872e39b1e0894bd673927e86ef8f031d4
kanfet <byte.developer@gmail.com>
2012-07-20 20:04:09 +0600

    mocking controller's specs; shoulda-mathchers in controller's specs

--- spec/controllers/phones_controller_spec.rb
+++ spec/controllers/phones_controller_spec.rb
@@ -3,70 +3,80 @@ require 'spec_helper'
 describe PhonesController do
 
   describe "GET #index" do
-    let(:nokia) {create(:nokia)}
     before(:each) do
-      @nokia_e51 = create(:nokia_e51)
-      create(:nokia_6089)
-      create(:asus_j101)
+      @phones = [mock_model(Phone), mock_model(Phone), mock_model(Phone)]
+      @phones.stub(:limit).and_return(@phones)
+      @phones.stub(:offset).and_return(@phones)
+      @phones.stub(:count).and_return(@phones.length)
+      @phones.each do |p|
+        p.stub(:as_json) do
+          {id: p.id, name: p.name, image: p.image}.to_json
+        end
+      end
+
+      Phone.stub(:by_params).and_return(@phones)
     end
 
     context "when request format is html" do
-      it "populates array of phones filtered by params" do
-        get :index, {vendor_id: nokia.id, contain_3g: "1"}
-        assigns(:phones).should == [@nokia_e51]
+
+      context "always" do
+        before(:each) do
+          get :index
+        end
+
+        it { should assign_to(:phones).with(@phones) }
+        it { should render_template :index }
+        it { should assign_to(:total_pages) }
       end
 
-      it "render :index view" do
-        get :index
-        response.should render_template :index
+      context "when page did not request" do
+        it do
+          get :index
+          should assign_to(:page).with(1)
+        end
       end
 
-      it "assigns requested page to @page" do
-        page = 2
-        get :index, page: page
-        assigns(:page).should == page
-      end
-
-      it "assigns 1 to @page when page did not request" do
-        get :index
-        assigns(:page).should == 1
-      end
-
-      it "assigns total pages to @total_pages" do
-        get :index
-        assigns(:total_pages).should == 1
+      context "when page requested" do
+        it do
+          @page = 2
+          get :index, page: @page
+          should assign_to(:page).with(@page)
+        end
       end
     end
 
     context "when request format is json" do
-      it "render array of phones filtered by params as json" do
-        get :index, {vendor_id: nokia.id, contain_3g: "1", format: :json}
-        response.body.should == {total_pages: 1, page: 1, phones: [@nokia_e51.as_json(only: [:id, :name, :image])]}.to_json
+      it "render array of phones as json" do
+        get :index, format: :json
+        phones_json = @phones.map do |p|
+          {id: p.id, name: p.name, image: p.image}.to_json
+        end
+        response.body.should == {total_pages: 1, page: 1, phones: phones_json}.to_json
       end
     end
   end
 
   describe "GET #show" do
-    let(:phone) {create(:phone)}
+    before(:each) do
+      @phone = mock_model(Phone)
+      Phone.stub(:find).and_return(@phone)
+      @phone.stub(:as_json) do
+        {id: @phone.id, name: @phone.name, image: @phone.image}
+      end
+    end
 
     context "when request format is html" do
-      it "assigns requested phone to @phone" do
-        get :show, id: phone
-        assigns[:phone].should == phone
+      before(:each) do
+        get :show, id: @phone
       end
-
-      it "render :show view" do
-        get :show, id: phone
-        response.should render_template(:show)
-      end
+      it { should assign_to(:phone).with(@phone) }
+      it { should render_template :show }
     end
 
     context "when request format is json" do
       it "render phone as json" do
-        get :show, {id: phone, format: :json}
-        response.body.should == phone.
-          as_json(include: [:vendor, :case_type, :phone_type, :screen_type, :touch_screen_type, :platform]).
-          to_json
+        get :show, {id: @phone, format: :json}
+        response.body.should == @phone.to_json
       end
     end
   end
--- spec/controllers/vendors_controller_spec.rb
+++ spec/controllers/vendors_controller_spec.rb
@@ -3,55 +3,54 @@ require 'spec_helper'
 describe VendorsController do
 
   describe "GET #index" do
-    it "populates an array of vendors" do
-      vendor = create(:vendor)
+    before(:each) do
+      @vendor = create :vendor
       get :index
-      assigns(:vendors).should == [vendor]
     end
 
-    it "renders the :index view" do
-      get :index
-      response.should render_template :index
-    end
+    it { should assign_to(:vendors).with([@vendor]) }
+    it { should render_template :index }
   end
 
   describe "GET #show" do
     before(:each) do
-      @nokia = create(:nokia)
-      @nokia_e51 = create(:nokia_e51)
-      @nokia_6089 = create(:nokia_6089)
-      create(:asus_j101)
+      @vendor = mock_model(Vendor)
+      Vendor.stub(:where).with(url: @vendor.url).and_return([@vendor])
+
+      @phones = [double(Phone), double(Phone), double(Phone)]
+      @phones.stub(:limit).and_return(@phones)
+      @phones.stub(:offset).and_return(@phones)
+      @phones.stub(:count).and_return(@phones.length)
+
+      Phone.stub(:by_vendor_url).with(@vendor.url).and_return(@phones)
     end
 
-    it "assigns requested vendor to @vendor" do
-      get :show, vendor_url: @nokia.url
-      assigns(:vendor).should == @nokia
+    context "always" do
+      before(:each) do
+        get :show, vendor_url: @vendor.url
+      end
+
+      it { should assign_to(:vendor).with(@vendor) }
+      it { should assign_to(:phones).with(@phones) }
+      it { should render_template :show }
+      it { should assign_to(:total_pages) }
     end
 
-    it "populates array of phones filtered by vendor url" do
-      get :show, vendor_url: @nokia.url
-      assigns(:phones).should == [@nokia_e51, @nokia_6089]
+    context "when page did not request" do
+      before(:each) do
+        get :show, vendor_url: @vendor.url
+      end
+
+      it { should assign_to(:page).with(1) }
     end
 
-    it "renders :show view" do
-      get :show
-      response.should render_template :show
-    end
+    context "when page requested" do
+      before(:each) do
+        @page = 2
+        get :show, vendor_url: @vendor.url, page: @page
+      end
 
-    it "assigns total pages to @total_pages" do
-      get :show, vendor_url: @nokia.url
-      assigns(:total_pages).should == 1
-    end
-
-    it "assigns requested page to @page" do
-      page = 2
-      get :show, page: page
-      assigns(:page).should == page
-    end
-
-    it "assigns 1 to @page when page did not request" do
-      get :show
-      assigns(:page).should == 1
+      it { should assign_to(:page).with(@page) }
     end
   end
 end
